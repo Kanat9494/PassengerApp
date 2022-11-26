a66688be39769fc43ca25ea766c8031c71c3ae46
Kanat9494 <kkanat94@gmail.com>
2022-11-27 00:51:11 +0600

    added new method

--- PassengerApp/PassengerApp/ViewModels/MainViewModel.cs
+++ PassengerApp/PassengerApp/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PassengerApp.Models.DTOs.Responses;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,5 +9,8 @@ namespace PassengerApp.ViewModels
     {
     }
 
-    internal async Task<IEnumerable<>>
+    internal async Task<IEnumerable<DriverLocationResponse>> GetNearByDrivers()
+    {
+
+    }
 }
