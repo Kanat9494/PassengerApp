b3b110012560ef9e7f50b646b8c61bc9ce35c541
Kanat9494 <kkanat94@gmail.com>
2022-11-27 00:59:47 +0600

    added new code to the service

--- PassengerApp/PassengerApp/Services/GetNearByDrivers.cs
+++ PassengerApp/PassengerApp/Services/GetNearByDrivers.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace PassengerApp.Services
@@ -9,9 +10,13 @@ namespace PassengerApp.Services
     {
         private static GetNearByDrivers _instance;
         private JsonSerializer _jsonSerializer = new JsonSerializer();
+        private HttpClient httpClient;
+
         public GetNearByDrivers() 
         { 
-
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://192.168.1.51:45455");
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
         }
     }
 }
