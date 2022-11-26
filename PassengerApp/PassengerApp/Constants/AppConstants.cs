a37b18844af2710b8cc2293fabcbb4732b2700bc
Kanat9494 <kkanat94@gmail.com>
2022-11-26 23:44:31 +0600

    set up main page

---
+++ PassengerApp/PassengerApp/Constants/AppConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PassengerApp.Constants
+{
+    public class AppConstants
+    {
+        //AIzaSyA5qwURk3rnuvsGTuKxoPNzzOzCArKjGfg
+    }
+}
